c17e400343d805157ad7027ecf788aeb4fe931a0
Danish49s1 <Danishsaifia49@gmail.com>
2025-10-19 16:21:48 +0000

    conatiner config done

---
+++ 01_basics/datetime.js
@@ -0,0 +1,8 @@
+//Date
+
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toLocaleString());
+
+let myCreatedDate = new Date(2003, 1, 17)
+console.log(myCreatedDate.toDateString());
