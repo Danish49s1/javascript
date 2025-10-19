f4992c247cedeec2dea731d9d269a74cc5ad8dfd
Danish49s1 <Danishsaifia49@gmail.com>
2025-10-19 10:10:20 +0000

    container config done

---
+++ 01_basics/string.js
@@ -0,0 +1,24 @@
+const name = "Danish"
+const height =  "6'2"
+
+//console.log('Hey I am ${name} and my height is ${height}');
+console.log(`Hey I am ${name} and my height is ${height}`);
+
+console.log(name.length);
+console.log(name.toUpperCase());
+console.log(name.charAt(6));
+console.log(name.indexOf('n'));
+
+//trim
+const anothername = "            Danish            "
+console.log(anothername);
+console.log(anothername.trim());
+
+//replace
+
+const url = "https://awsdrproject.cloud/20/aws/services/2025"
+console.log(url.replace('aws', 'AWS'));
+console.log(url.replace('.cloud', '.google'));
+
+
+
